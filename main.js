0114414cf92ee6cb86e49c5c086c6d0a5774562c
ctovarg81893@universidadean.edu.co <ctovarg81893@universidadean.edu.co>
2019-05-19 21:13:14 -0500

    V. 2

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 alert('VENTANA JAVA: Saludos!');
+
+function() barcelona{
+barcelona = document.getElementById('myImage').src='pic_bulbon.gif';
+}
+
+function() madrid{
+madrid = document.getElementById('myImage').src='pic_bulboff.gif'
+}
